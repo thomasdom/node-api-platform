85cbdc56adc85077d13661a2175b539820fb1ed7
Romain <bigzounet@hotmail.com>
2018-08-17 13:17:33 +0200

    disable features if peer devs are not installed

--- server/initWebpackDashboard.js
+++ server/initWebpackDashboard.js
@@ -1,23 +1,31 @@
-const webpack = require('webpack');
-const dashboardPlugin = require('webpack-dashboard/plugin');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const defaultConfig = require('./webpack.config.js');
-
 /**
  * Adds a webpack middleware to the server to use webpack-dashboard in dev mode.
  *
  * @param server Hapi server
  * @param config Webpack config
+ * @return boolean true on success, false on failure
  */
 module.exports = (server, config = {}) => {
-  const compiler = webpack(Object.assign({}, defaultConfig, config));
-  compiler.apply(new dashboardPlugin());
-  const devMiddleware = webpackDevMiddleware(
-    compiler,
-    { host: server.info.host, port: server.info.port, quiet: true }
-  );
-  server.ext({
-    type: 'onRequest',
-    method: (request, h) => devMiddleware(request.raw.req, request.raw.res, () => h.continue)
-  });
+  // Don't use this feature if webpack-dashboard requirements are not installed
+  try {
+    const webpack = require('webpack');
+    const dashboardPlugin = require('webpack-dashboard/plugin');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const defaultConfig = require('./webpack.config.js');
+
+    const compiler = webpack(Object.assign({}, defaultConfig, config));
+    compiler.apply(new dashboardPlugin());
+    const devMiddleware = webpackDevMiddleware(
+      compiler,
+      { host: server.info.host, port: server.info.port, quiet: true }
+    );
+    server.ext({
+      type: 'onRequest',
+      method: (request, h) => devMiddleware(request.raw.req, request.raw.res, () => h.continue)
+    });
+
+    return true;
+  } catch (error) {
+    return false;
+  }
 };
--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,9 +1,13 @@
-var nodeExternals = require('webpack-node-externals');
+try {
+  var nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-  context: __dirname + "/app",
-  mode: "development",
-  entry: '../../../../app/server.js',
-  target: "node",
-  externals: [nodeExternals()]
-};
+  module.exports = {
+    context: __dirname + "/app",
+    mode: "development",
+    entry: '../../../../app/server.js',
+    target: "node",
+    externals: [nodeExternals()]
+  };
+} catch (e) {
+  
+}
