468a4747b44fa2449603783226ad61a3d3c75703
Romain <bigzounet@hotmail.com>
2018-08-17 20:45:54 +0200

    add jsonapi first features

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+// Server
 exports.initWebpackDashboad = require('./server/initWebpackDashboard');
 exports.initSwaggerHapi = require('./server/initSwaggerHapi');
 exports.loadRoutes = require('./server/loadRoutes');
 exports.environments = require('./server/environments');
+
+// JsonApi
+exports.jsonApiConstants = require('./jsonapi/constants');
+exports.jsonApiJoiFormatter = require('./jsonapi/joiFormatter');
---
+++ jsonapi/constants.js
@@ -0,0 +1,7 @@
+const jsonApiHeader = 'application/vnd.api+json';
+module.exports = {
+  HEADERS: {
+    'Content-Type': jsonApiHeader,
+    'Accept': jsonApiHeader
+  }
+};
